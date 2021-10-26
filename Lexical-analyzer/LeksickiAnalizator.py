3b9a994357cddaa9f665e663dccbb7a845005d9a
DM <70340368+domamaric@users.noreply.github.com>
2021-10-26 17:01:02 +0200

    Automata implemented, changed comment remving

--- Lexical-analyzer/LeksickiAnalizator.py
+++ Lexical-analyzer/LeksickiAnalizator.py
@@ -2,15 +2,37 @@ from sys import stdin
 
 
 class Automata:
-    @staticmethod
-    def simulate_automata(str_of_sth, line_no):
-        curr_state = "S"
-        if curr_state == "S":
-            print(str_of_sth, line_no)
-        elif curr_state == "IDN":
-            pass
-        elif curr_state == "KON":
-            pass
+    def __init__(self):
+        self.state = "S"
+        self.curr_value = ""
+
+    def simulate_automata(self, str_of_sth, index, line_no):
+        for c in str_of_sth:
+            if self.state == "S":
+                if c.isdigit():
+                    self.state = "KON"
+                else:
+                    self.state = "IDN"
+                self.curr_value += c
+            elif self.state == "IDN":
+                if c.isalnum():
+                    self.curr_value += c
+                else:
+                    print("{} {} {}".format(DataTypes.IDENTIFIER, line_no, self.curr_value))
+                    return index
+            elif self.state == "KON":
+                if c.isdigit():
+                    self.curr_value += c
+                else:
+                    print("{} {} {}".format(DataTypes.NUMBER, line_no, self.curr_value))
+                    return index
+            index += 1
+        if self.state == "IDN":
+            print("{} {} {}".format(DataTypes.IDENTIFIER, line_no, self.curr_value))
+        elif self.state == "KON":
+            print("{} {} {}".format(DataTypes.NUMBER, line_no, self.curr_value))
+
+        return index
 
 
 class DataTypes:
@@ -34,38 +56,43 @@ class DataTypes:
 
 
 class Analyzer:
-    def __init__(self, line_no=0, pos=-1):
-        self.line_no = line_no
-        self.pos = pos
+    def __init__(self):
+        self.line_no = -1
 
-    def analyze_lines(self, lines: list):
+    def analyze_prog(self, lines: list):
         # tpl -> tuple(int, str), pair of line number and instruction
         for tpl in lines:
             self.line_no = tpl[0]
-            str_of_sth = "".join(tpl[1].split()) # Remove all the whitespace
+            line = tpl[1].split()
 
-            if not str_of_sth:  # Line is empty, do nothing
-                continue
-            # print("{} {}".format(self.line_no, str_of_sth))
-            # Moram razbit words u leksicke jedinke
-            Automata.simulate_automata(str_of_sth, self.line_no)
+            if line:  # Line is empty, do nothing
+                self.analyze_line(line)
 
+    def analyze_line(self, line):
+        for s in line:
+            if s in DataTypes.KEYWORDS.keys():
+                print("{} {} {}".format(DataTypes.KEYWORDS[s], self.line_no, s))
+            else:
+                i = 0
+                while i < len(s):
+                    if s[i] in DataTypes.OPERATORS.keys():
+                        print("{} {} {}".format(DataTypes.OPERATORS[s[i]], self.line_no, s[i]))
+                        i += 1
+                    else:
+                        temp = s[i:]
+                        # print(temp)
+                        automata = Automata()
+                        i = automata.simulate_automata(temp,i, self.line_no)
 
     @staticmethod
     def remove_comments(lines: list):
         commentless_prog = []
         for line in lines:
-            if line.startswith('//'):
-                # Remove comments from the begging of line
-                commentless_prog.append('')
-            elif '//' in line:
-                # Remove comment in the middle of line
-                index = line.index('//')
-                line = line[:index]
-                commentless_prog.append(line)
-            else:
-                # No comments in line, append line
-                commentless_prog.append(line)
+            try:
+                ind_of_comm = line.index('//') # Ako postoji ostat će ovde
+                commentless_prog.append(line[:ind_of_comm])
+            except ValueError: # Dakle nema comentara u liniji
+                commentless_prog.append(line.rstrip())
 
         return list(enumerate(commentless_prog, 1))
 
@@ -76,7 +103,7 @@ def main():
 
     lex = Analyzer()
     data = lex.remove_comments(data)
-    lex.analyze_lines(data)
+    lex.analyze_prog(data)
 
 
 if __name__ == '__main__':
